478fc0de7b35ad55f1330518c0105f1972d87d02
Tiago <tiagosantos0412@gmail.com>
2023-11-26 22:28:56 -0300

    Configurando a exibição do template de acordo com a model criada

--- nivelamento_django/nivelamento_django/app/views.py
+++ nivelamento_django/nivelamento_django/app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from  .forms import TarefaForm
 
 # Create your views here.
 
 def cadastrar_tarefa(request):
-    return render(request, 'form_cadastro.html')
+    form_tarefa = TarefaForm()
+    return render(request, 'form_cadastro.html', {'form_tarefa':form_tarefa})
