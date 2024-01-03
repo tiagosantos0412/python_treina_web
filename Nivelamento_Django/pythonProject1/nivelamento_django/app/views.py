5ef429ad8c31f21b0390511dab1f1c2bd6d0624d
Tiago Felipe dos Santos <tiago.santos@unihosp.local>
2024-01-03 15:44:59 -0300

    Criando e renderizando o formulario e salvando a tarefa no sqlite

--- Nivelamento_Django/pythonProject1/nivelamento_django/app/views.py
+++ Nivelamento_Django/pythonProject1/nivelamento_django/app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .forms import TarefaForm
 
 # Create your views here.
+
+def cadastrar_tarefa(request):
+    if request.method == "POST":
+        form_tarefa = TarefaForm(request.POST)
+        if form_tarefa.is_valid():
+            form_tarefa.save()
+    else:
+        form_tarefa = TarefaForm()
+    return render(request, 'form_cadastro.html', {'form_tarefa': form_tarefa})
