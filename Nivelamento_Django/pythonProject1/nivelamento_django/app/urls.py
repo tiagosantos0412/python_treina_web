a662d4377a9d43d86160cbc80ac642c384d0fa2c
Tiago Felipe dos Santos <31144825+tiagosantos0412@users.noreply.github.com>
2024-01-17 11:21:33 -0300

    Delete Nivelamento_Django/pythonProject1 directory

--- Nivelamento_Django/pythonProject1/nivelamento_django/app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import cadastrar_tarefa, listar_tarefas
-urlpatterns = [
-    path('cadastrar_tarefa', cadastrar_tarefa, name='cadastrar_tarefa'),
-    path('listar_tarefas', listar_tarefas, name='listar_tarefas')
-]
